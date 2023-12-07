07fff7209076c900efa796d543c8e82f22d3a537
Rhys Johnston <rhysjj111@gmail.com>
2023-12-07 23:01:37 +0000

    add routes to add_driver.html. add a route to accept new driver entries.

--- wages_calculator/routes.py
+++ wages_calculator/routes.py
@@ -5,4 +5,20 @@ from wages_calculator.models import Driver, Day_end
 
 @app.route("/")
 def home():
-    return render_template("driver_entry.html")
+    return render_template("data_entry.html")
+
+@app.route("/add_driver", methods=["GET", "POST"])
+def add_driver():
+    drivers = list(Driver.query.all())
+    if request.method == "POST":
+        driver = Driver(
+            start_date=request.form.get("start_date"),
+            first_name=request.form.get("first_name"),
+            second_name=request.form.get("second_name"),
+            base_wage=request.form.get("base_wage"),
+            bonus_percentage=request.form.get("bonus_percentage")
+            )
+        db.session.add(driver)
+        db.session.commit()
+    return render_template("add_driver.html", drivers=drivers)
+
