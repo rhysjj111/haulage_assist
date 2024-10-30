b4882e47df2a5630d2cf6a9f95a4efcd5d7f580b
Rhys Johnston <rhysjj111@gmail.com>
2024-10-30 16:12:03 +0000

    Update wages calculator route. Replace calculations with function.

--- haulage_app/wages_calculator/routes.py
+++ haulage_app/wages_calculator/routes.py
@@ -1,65 +1,29 @@
 from flask import render_template, request
 from haulage_app import db, f
-from haulage_app.models import Driver, Day, Job
+from haulage_app.models import Driver, Day, Job, Payslip
 from datetime import timedelta, date, datetime
 from haulage_app.wages_calculator import wages_calculator_bp
+from haulage_app.calculations.driver_truck_metrics import (
+    calculate_driver_metrics_week,)
+from haulage_app.functions import (
+        get_week_number_sat_to_fri,
+        get_start_end_of_week)
 from pprint import pprint
 
 @wages_calculator_bp.route("/wages_calculator", methods=["GET"])
 def wages_calculator():
     drivers = list(Driver.query.order_by(Driver.first_name).all())
 
-    today = datetime.utcnow().date()
-    start_date = today - timedelta(days=(today.weekday() + 2) % 7)
-    end_date = start_date + timedelta(days=6)
+    today_year, today_week = get_week_number_sat_to_fri(date.today())
+    start_date, end_date = get_start_end_of_week(today_year, today_week)
 
     driver_data = {}
 
     for driver in drivers:
-        day_entries = Day.query.filter(
-            Day.driver_id == driver.id, 
-            Day.date >= start_date, 
-            Day.date <= end_date
-            ).order_by(Day.date).all()
+
+        driver_data[driver.id] = calculate_driver_metrics_week(
+            driver, Day, Job, Payslip, start_date, end_date)
         
-        job_entries = Job.query.join(Day).filter(
-            Day.driver_id == driver.id,
-            Day.date >= start_date,
-            Day.date <= end_date
-            ).order_by(Day.date, Job.id).all()
-
-        # Calculate wages for each driver
-        weekly_total_earned = 0
-        weekly_total_overnight = 0
-        weekly_total_bonus_wage = 0
-        weekly_total_wage = 0
-        weekly_bonus = 0
-        for day in day_entries:
-            day.daily_bonus = day.calculate_daily_bonus(driver)
-            weekly_total_bonus_wage += day.daily_bonus
-            weekly_total_earned += day.calculate_total_earned()
-            if day.overnight == True:
-                weekly_total_overnight += 3000
-        if weekly_total_earned > driver.weekly_bonus_threshold:
-            weekly_bonus = (weekly_total_earned - driver.weekly_bonus_threshold) * driver.weekly_bonus_percentage
-            weekly_total_bonus_wage += weekly_bonus
-        weekly_extras = weekly_total_bonus_wage - (15000 - weekly_total_overnight)
-        weekly_total_wage = weekly_extras + driver.basic_wage
-
-        driver_data[driver.id]={
-            'driver': driver,
-            'day_entries': day_entries,
-            'weekly_total_earned': weekly_total_earned,
-            'weekly_total_overnight': weekly_total_overnight,
-            'weekly_total_bonus_wage': weekly_total_bonus_wage,
-            'weekly_total_wage': weekly_total_wage,
-            'weekly_bonus': weekly_bonus,
-            'weekly_extras': weekly_extras,
-            'start_date': start_date,
-            'end_date': end_date,
-            'job_entries': job_entries
-        }
-
-    pprint(driver_data)
+        pprint(driver_data)
 
     return render_template("wages_calculator.html", driver_data=driver_data, drivers=drivers, start_date=start_date, end_date=end_date)
