ee284213e38f8b7c3b1d0c57a025e6a78e5a8220
Rhys Johnston <rhysjj111@gmail.com>
2024-11-02 13:06:38 +0000

    Update update_working_values - uncommented code so that it does the intended funtion.

--- update_day.py
+++ update_day.py
@@ -7,8 +7,6 @@ with app.app_context():
 
     days = Day.query.filter(Day.status != 'working').all()
     for day in days:
-        pprint(day.truck_id)
         day.truck_id = None
-        pprint(day.truck_id)
     db.session.commit()
 
--- update_working_values.py
+++ update_working_values.py
@@ -6,11 +6,11 @@ from pprint import pprint
 with app.app_context():
 
     days = Day.query.all()
-    # for day in days:
-    #     pprint(day.status)
-    #     if day.status == 'Working':
-    #         day.status = 'working'
-    #     pprint(day.status)
-        # db.session.commit()
+    for day in days:
+        pprint(day.status)
+        if day.status == 'Working':
+            day.status = 'working'
+        db.session.commit()
+    pprint('completed')
     for day in days:
         pprint(day.status)
