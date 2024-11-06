62d62e50ad7b6bda00fccc61788b8319b3b2f163
Rhys Johnston <rhysjj111@gmail.com>
2024-11-06 16:24:15 +0000

    Update update_day.py fileto assign truck to driver 1.

--- update_day.py
+++ update_day.py
@@ -7,6 +7,7 @@ with app.app_context():
 
     days = Day.query.filter(Day.status != 'working').all()
     for day in days:
-        day.truck_id = None
+        if day.driver_id == 1:
+            day.truck_id = 3
     db.session.commit()
 
