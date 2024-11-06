30688fa0ebd9ab72b9e561050f38f939bb7ffdac
Rhys Johnston <rhysjj111@gmail.com>
2024-11-06 16:40:24 +0000

    update update_day.py

--- update_day.py
+++ update_day.py
@@ -8,8 +8,16 @@ with app.app_context():
     days = Day.query.filter(Day.status == 'working').all()
     for day in days:
         if day.driver_id == 1:
-            print(day.truck_id)
+            print("old value for", day.driver.first_name, day.truck_id)
             day.truck_id = 3
-            print(day.truck_id, "sa;fjkh")
+            print("NEW value for", day.driver.first_name, day.truck_id)
+        if day.driver_id == 2:
+            print("old value for", day.driver.first_name, day.truck_id)
+            day.truck_id = 1
+            print("NEW value for", day.driver.first_name, day.truck_id)
+        if day.driver_id == 3:
+            print("old value for", day.driver.first_name, day.truck_id)
+            day.truck_id = 2
+            print("NEW value for", day.driver.first_name, day.truck_id)
     # db.session.commit()
 
